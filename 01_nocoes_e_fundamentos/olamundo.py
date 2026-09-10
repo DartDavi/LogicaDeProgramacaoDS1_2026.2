412509f010c5a07d40d58d144e4a75daa8e7846a
DartDavi <davidesouzalima02@gmai.com>
2026-09-10 16:15:24 -0300

    Nome idade teste

--- 01_nocoes_e_fundamentos/olamundo.py
+++ 01_nocoes_e_fundamentos/olamundo.py
@@ -1,2 +1,8 @@
-print("Davi de Souza")
-print("Olá, Mundo")
+# print("Davi de Souza")
+# print("Olá, Mundo")
+
+nome = input("Digite seu nome: ")
+dia = input("Digite o dia em que nasceu: ")
+mes = input("Digite seu mes de nascimento: ")
+ano = input("Digite em que ano você nasceu: ")
+print (f"Seu nome é: {nome}, Nasceu dia: {dia}, do mes: {mes}, do ano: {ano}")
